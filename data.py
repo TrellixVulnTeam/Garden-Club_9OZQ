8f53bc7978d72f3d195f92b453d2e9a265b114eb
Preston Turner Jr <preston.turner.jr@gmail.com>
2019-03-18 05:26:18 -0500

    adding personal project

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': "Article 1",
+            'body': 'lorem somehting',
+            'author': 'Preston',
+            'createDate': '03-18-2019'
+        },
+                {
+            'id': 2,
+            'title': "Article 2",
+            'body': 'lorem somehting',
+            'author': 'francis',
+            'createDate': '03-18-2019'
+        },
+                {
+            'id': 3,
+            'title': "Article 3",
+            'body': 'lorem somehting',
+            'author': 'Lisa',
+            'createDate': '03-18-2019'
+        }
+    ]
+    return articles
